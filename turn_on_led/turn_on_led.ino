3f197128c38f6aa7b981a4e5a6bd1c77c446f080
deeplearningpc <yyuanli19@gmail.com>
2021-04-20 17:05:54 +0200

    changed script with keyboard control

--- turn_on_led/turn_on_led.ino
+++ turn_on_led/turn_on_led.ino
@@ -1,16 +1,26 @@
+// number of the pins should be assigned here
 int Led = 3;
+int Baud_rate = 9600;
 
 void setup() {
   // initialize digital pin LED as an output.
   pinMode(Led, OUTPUT);
-
+  Serial.begin(Baud_rate);
 }
 
 // the loop function runs over and over again forever
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(Led, 1);   // turn the LED on (HIGH is the voltage level)
-  delay(100);
-  digitalWrite(Led, 0); 
-  delay(100);
+  if (Serial.available() > 0) {
+    int command = Serial.read();
+    if (command == '1') {
+      digitalWrite(Led, 1); 
+      Serial.println("LED on");
+    } else if (command == '2') {
+      digitalWrite(Led, 0); 
+      Serial.println("LED off");
+    } else {
+      Serial.println("unknown command, please only press 1 or 2");
+    } 
+  }
 }
